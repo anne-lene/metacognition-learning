ca4b345fd18a4837bac37698f66f044982dfeadc
Anne-Lene Sax <as17567@bristol.ac.uk>
2023-05-24 16:08:58 +0200

    "Stop tracking file"

--- fixed_feedback/notebooks/utility_functions.py
+++ fixed_feedback/notebooks/utility_functions.py
@@ -27,6 +27,26 @@ def correct_condition(df):
     df.loc[(df.trial==11) & (df.block==3), 'condition']='baseline'
     return df
 
+
 def standardise(df, var):
     df[f'{var}_c']=(df[f'{var}']-df[f'{var}'].mean())/df[f'{var}'].std()
     return df
+
+
+def define_groups(df, x):
+    df.loc[df[x]<=1, 'group_split_2']=1
+    df.loc[df[x]>=9, 'group_split_2']=2
+    return df
+
+
+def return_bdi_group(score):
+    if 0 <= score < 4:
+        return 1
+    elif 4 <= score < 9:
+        return 2
+    elif 9 <= score < 13:
+        return 3
+    elif 13 <= score <= 22:
+        return 4
+    else: 
+        print('Score not found')
